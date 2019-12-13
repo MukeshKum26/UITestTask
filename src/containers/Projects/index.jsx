71d85e00d6239a4fa974c2984fc78b5f74bd67d0
mmpatel <mkdev244@gmail.com>
2019-12-13 17:55:01 +0530

    Added projects container

---
+++ src/containers/Projects/index.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import TableComponent from '../../components/Table'
+import { fetchProjectList } from '../../redux-saga/actions/projectsList'
+import { columns } from './data'
+
+class ProjectsList extends Component {
+  componentDidMount() {
+    const { props: { getProjects }} = this
+    getProjects()
+  }
+
+  handleDelete = (id) => {
+    console.log("asdasdasdasd", id)
+  }
+
+  dataFormatter = (rows) => (
+    rows.map(row => ({
+      ...row,
+      name: {
+        label: row.name,
+        url: `/project/${row.id}`
+      },
+      repoUrl: {
+        label: row.repoUrl,
+        url: `/project/${row.id}`
+      },
+      liveUrl: {
+        label: row.liveUrl,
+        url: `/project/${row.id}`
+      },
+      action: {
+        label: 'Delete',
+        onClick:() => this.handleDelete(row.id)
+      }
+    }))
+  )
+
+  render() {
+    const { projectsList: { err, result } } = this.props
+    const rows = this.dataFormatter( result )
+    if (result && result.length > 0) {
+      return (
+        <TableComponent items={rows} columns={columns} />
+      )
+    } else if (err) {
+      return <div> Error </div>
+    }
+    return (
+      <div> Loading </div>
+    )
+  }   
+}
+
+const mapDispatchToProps = dispatch => ( {
+  getProjects: () => dispatch(fetchProjectList())
+} )
+
+const mapStateToProps = ({ projectsList }) => ({
+  projectsList
+})
+
+export default connect( mapStateToProps, mapDispatchToProps )(ProjectsList)
