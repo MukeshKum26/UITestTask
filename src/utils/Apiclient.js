a6a4dc594f5613c1b0ac5a4e9d6b611148e5bb61
mmpatel <mkdev244@gmail.com>
2019-12-16 22:56:15 +0530

    Added header and axios requests

--- src/utils/Apiclient.js
+++ src/utils/Apiclient.js
@@ -2,8 +2,14 @@ import { getData, postData, deleteData, putData } from './axios'
 
 export const getStudendsList = () => getData('http://localhost:12345/api/v1/student') //env
 
-export const deleteStudent = (id) => deleteData(`http://localhost:12345/api/v1/student/${id}`)
+export const deleteStudent = id => deleteData(`http://localhost:12345/api/v1/student/${id}`)
 
 export const getProjectsList = () => getData('http://localhost:12345/api/v1/projects') //env
 
-export const deleteProject = (id) => deleteData(`http://localhost:12345/api/v1/projects/${id}`)
+export const deleteProject = id => deleteData(`http://localhost:12345/api/v1/projects/${id}`)
+
+export const getStudentDetails = id => getData(`http://localhost:12345/api/v1/student/${id}`)
+
+export const addStudentDetails = data => postData(`http://localhost:12345/api/v1/student/`, data)
+
+export const updateStudentDetails = data => putData(`http://localhost:12345/api/v1/student/${data.id}`, data)
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
+axios.defaults.headers.common['Authorization'] = "Bearer b1e62ff9-bd22-414b-8060-6w1210c1f167"
+
 export const getData = ( url, params ) => axios.get( url, params )
 
-export const postData = ( { url, body } ) => axios.post( url, body )
+export const postData = ( url, body ) => axios.post( url, body )
 
-export const putData = ( { url, body }) => axios.put( url, body )
+export const putData = ( url, body ) => axios.put( url, body )
 
 export const deleteData = ( url ) => axios.delete( url )
